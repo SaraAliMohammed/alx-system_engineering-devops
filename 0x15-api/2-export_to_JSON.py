ea5f82c1b4cfced67553a302d37ae8056cf69ccb
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-02-19 04:55:16 -0800

    Task3. Dictionary of list of dictionaries

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+"""Exports to-do list information for a given employee ID to json format."""
 import json
 import requests
 import sys
