26948838923e12b23661f735f298382aa8592ea7
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-02-19 04:50:48 -0800

    Task2. Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to CSV format."""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    filename = "{}.json".format(user_id)
+    with open(filename, "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": username
+            } for todo in todos]}, jsonfile)
