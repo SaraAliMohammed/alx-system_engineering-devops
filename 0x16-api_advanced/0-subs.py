ce720f99706ecfd5abb13aa0370e5cf3d79ebd06
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-03-05 02:56:16 -0800

    Task0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,15 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    headers={'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/sara_ali2024)'}
+    # Make a GET request to the API with a custom User-Agent
+    data = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if the request was successful (status code 200)
+    if data.status_code == 200:
+        # Parse the JSON response to get the number of subscribers
+        return data.json().get('data').get('subscribers')
+    else:
+        # Invalid subreddit or other error, return 0
+        print(f"Error: {data.status_code}")
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
