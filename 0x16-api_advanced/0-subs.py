e7a995c410b4bf4caa26a46b503577d256c9e15d
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-03-05 02:31:08 -0800

    Task0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,9 +12,8 @@ def number_of_subscribers(subreddit):
         "User-Agent": "linux:0x16.api.advanced"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    if response.status_code == 404:
         return 0
+    data = response.json()
+    subscribers = data['data']['subscribers']
+    return subscribers
