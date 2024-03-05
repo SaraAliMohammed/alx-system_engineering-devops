d2add88ae4421520b2a4aaf72ba51027ef2c81a8
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-03-05 02:27:21 -0800

    Task0. How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced"
+    }
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0
+    except Exception as e:
+        print("An error occurred:", e)
+        return 0
