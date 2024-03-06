2492b15e0c2b77e2f6eb10cc6418ee5849f01ac2
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-03-06 01:51:21 -0800

    Task0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-"""Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0."""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/sara_ali2024)"
-    }
-    # Make a GET request to the API with a custom User-Agent
-    data = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the request was successful (status code 200)
-    if data.status_code == 200:
-        # Parse the JSON response to get the number of subscribers
-        return data.json().get('data').get('subscribers')
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        # Invalid subreddit or other error, return 0
-        print(f"Error: {data.status_code}")
         return 0
